84d1af9236c3a4b0eb35bf35e2693a09b08e9145
alfredo-petri <alfredo_petri@hotmail.com>
2025-06-11 18:26:53 -0300

    feat: add subtraction method

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.Clear();
-            Sum();
+            Subtraction();
         }
 
         static void Sum()
@@ -24,6 +24,25 @@
 
             Console.WriteLine($"O resultado da operação é {result}");
             
+            Console.WriteLine("");
+        }
+        
+        static void Subtraction()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Digite o primeiro valor: ");
+            float value1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor: ");
+            float value2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float result = value1 - value2;
+
+            Console.WriteLine($"O resultado da operação é {result}");
+
             Console.WriteLine("");
         }
     }
