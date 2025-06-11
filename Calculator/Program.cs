721ec5f04d09f8cb5eeecb7e0ae85a8febeae01a
alfredo-petri <alfredo_petri@hotmail.com>
2025-06-11 18:19:14 -0300

    refactor: add sum method

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -5,13 +5,26 @@
         static void Main(string[] args)
         {
             Console.Clear();
+            Sum();
+        }
+
+        static void Sum()
+        {
+            Console.Clear();
+
             Console.WriteLine("Digite o primeiro valor: ");
             float value1 = float.Parse(Console.ReadLine());
+
             Console.WriteLine("Digite o segundo valor: ");
             float value2 = float.Parse(Console.ReadLine());
+
             Console.WriteLine("");
+
             float result = value1 + value2;
+
             Console.WriteLine($"O resultado da operação é {result}");
+            
+            Console.WriteLine("");
         }
     }
 }
