69116389a8dbcf95851333fa6a332f26d66f394f
alfredo-petri <alfredo_petri@hotmail.com>
2025-06-11 18:29:36 -0300

    feat: add division method

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.Clear();
-            Subtraction();
+            Division();
         }
 
         static void Sum()
@@ -40,6 +40,24 @@
             Console.WriteLine("");
 
             float result = value1 - value2;
+
+            Console.WriteLine($"O resultado da operação é {result}");
+
+            Console.WriteLine("");
+        }
+        static void Division()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Digite o primeiro valor: ");
+            float value1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor: ");
+            float value2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float result = value1 / value2;
 
             Console.WriteLine($"O resultado da operação é {result}");
 
